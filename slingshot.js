38ffb7584eae53d4a73896f2960a362ef17838b4
purvi-patidar <64897897+purvi-patidar@users.noreply.github.com>
2020-07-04 09:44:16 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,32 @@
+class Chain{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.1,
+            length:30
+        }
+        this.pointB=pointB
+        this.launcher = Constraint.create(options);
+        World.add(world, this.launcher);
+    }
+
+    fly(){
+    this.launcher.bodyA=null
+    }
+
+    display(){
+
+        if(this.launcher.bodyA){
+        var pointA = this.launcher.bodyA.position;
+        var pointB = this.launcher.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+
+        }    
+    }
+
+    
+
+    
+}
